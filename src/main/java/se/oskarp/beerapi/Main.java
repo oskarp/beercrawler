b53cdc6108765f6d1d42f0405faf092aed6f748e
Martin <martin@arya>
2015-06-26 20:45:41 +0200

    made the application executable

---
+++ src/main/java/se/oskarp/beerapi/Main.java
@@ -0,0 +1,12 @@
+package se.oskarp.beerapi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("--- Begin execution          ---");
+
+        System.out.println("--- fancy stuff goes on here ---");
+
+        System.out.println("--- End execution            ---");
+    }
+}
