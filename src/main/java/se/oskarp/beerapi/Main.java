c7034f7e50067a326b09fdc888306b0aa33bfb15
Martin <martin@arya>
2015-06-26 21:46:54 +0200

    #1 guice dependency added + a crude configuration file implementation

--- src/main/java/se/oskarp/beerapi/Main.java
+++ src/main/java/se/oskarp/beerapi/Main.java
@@ -1,11 +1,17 @@
 package se.oskarp.beerapi;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("--- Begin execution          ---");
 
-        System.out.println("--- fancy stuff goes on here ---");
+        Injector injector = Guice.createInjector(new Configuration());
+
+        JustForShow justForShow = injector.getInstance(JustForShow.class);
+        System.out.println("url: " + justForShow.getBolagetApiUrl());
 
         System.out.println("--- End execution            ---");
     }
