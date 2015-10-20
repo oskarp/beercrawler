1f59e1ff1c36dd6a1872f268a23d9e773df42852
oskarp <oskar@derived.se>
2015-10-20 22:43:39 +0200

    Refined test
    The test now simulates a create -> update -> delete cycle of a single
    beer.

--- src/test/java/se/oskarp/beerapi/infrastructure/event/EventRepositoryRESTTest.java
+++ src/test/java/se/oskarp/beerapi/infrastructure/event/EventRepositoryRESTTest.java
@@ -17,24 +17,34 @@ public class EventRepositoryRESTTest {
 
     @Test
     //@Ignore
+
+    // Test now creates a beer, updates it and then deletes it.
     public void save() throws Exception {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 
         EventRepositoryREST repo = new EventRepositoryREST("http://homestead.app/event");
 
-        Beer b1 = new Beer("Åbro orginal", "Smakar lite som en säl luktar.", "Lager", 1483, 22.3, "Åbro", "Sverige", "", 6, "flaska", "öl", true, 50, formatter.parse("2005-10-01"), "Åbro", "", 1483);
+        Beer b1 = new Beer("Åbro orginal", "Smakar lite som en säl luktar.", "Lager", 1483, 22.3, "Åbro", "Sverige", "", 6, "flaska", "öl", true, 50, formatter.parse("2005-10-01"), "Åbro", "orginalet", 1483);
         Beer b2 = new Beer("Norrlands guld", "Hopps", "Lager", 14127, 19.5, "Spendrups", "Sverige", "", 5, "flaska", "öl", true, 330, formatter.parse("2005-10-01"), "Spendrups", "", 14127);
-        Beer b3 = new Beer("Åbro Orginal", "Damm", "Lager", 1483, 23.2, "Åbro", "England", "", 5.2, "flaska", "öl", true, 50, formatter.parse("2005-10-01"), "Åbro", "", 1483);
+        Beer b3 = new Beer("Åbro Orginal", "Damm", "Lager", 1483, 23.2, "Åbro", "England", "", 5.2, "flaska", "öl", true, 50, formatter.parse("2005-10-01"), "Åbro", "orginalet", 1483);
         Beer b4 = new Beer("Falcon", "Njet", "Lager", 95483, 20, "Falcon", "Sverige", "", 4.8, "flaska", "öl", true, 335, formatter.parse("1999-10-01"), "Spendrups", "", 95483);
-
+        /*
         Event e1 = new Event(1483, Event.Action.Update, b1, b3);
         Event e2 = new Event(14127, Event.Action.Create, new Beer(), b2);
         Event e3 = new Event(95483, Event.Action.Delete, b4, new Beer());
+        */
+
+
+        Event en1 = new Event(1483, Event.Action.Create, new Beer(), b1);
+        Event en2 = new Event(1483, Event.Action.Update, b1, b3);
+        Event en3 = new Event(1483, Event.Action.Delete, b3, new Beer());
+
+
 
         List<Event> eventList = new ArrayList<>();
-        eventList.add(e1);
-        eventList.add(e2);
-        eventList.add(e3);
+        eventList.add(en1);
+        eventList.add(en2);
+        eventList.add(en3);
         repo.save(eventList);
     }
 }
